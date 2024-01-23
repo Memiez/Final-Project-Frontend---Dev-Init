200891748c8fef60a141af1362e5d9004ec207e1
Memiez <iam_minny@hotmail.com>
2024-01-23 22:32:22 +0700

    /**

---
+++ src/components/DragAndDrop.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <draggable :list="items">
+        <div v-for="(item, index) in items" :key="index">{{ item }}</div>
+      </draggable>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import draggable from "vuedraggable";
+  
+  export default {
+    components: {
+      draggable,
+    },
+    data() {
+      return {
+        items: ["Item 1", "Item 2", "Item 3"],
+      };
+    },
+  };
+  </script>
