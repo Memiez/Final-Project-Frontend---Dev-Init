79bc87f2ff73c4a4ca7c5c20d442bebf970f8fa8
Memiez <iam_minny@hotmail.com>
2024-01-23 23:11:41 +0700

    com

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,22 +1,15 @@
 <template>
-    <div>
-      <draggable :list="items">
-        <div v-for="(item, index) in items" :key="index">{{ item }}</div>
-      </draggable>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import draggable from "vuedraggable";
-  
-  export default {
-    components: {
-      draggable,
-    },
-    data() {
-      return {
-        items: ["Item 1", "Item 2", "Item 3"],
-      };
-    },
-  };
-  </script>
+  <div>
+    <draggable :list="items">
+      <div v-for="(item, index) in items" :key="index">{{ item }}</div>
+    </draggable>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+
+// สร้างตัวแปร reactive โดยใช้ ref
+const items = ref(['Item 1', 'Item 2', 'Item 3'])
+</script>
